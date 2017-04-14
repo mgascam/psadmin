8ba96dc6fac5f11881d181c0a55552e9c00b7c6a
Miguel Gasca <miguelgasca@gmail.com>
2017-04-14 14:30:15 +0200

    Home Page react component

---
+++ src/components/homePage.js
@@ -0,0 +1,19 @@
+/**
+ * Created by miguel on 14/04/17.
+ */
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function () {
+        return (
+            <div className="jumbotron">
+                <h1>Pluralsight Administration</h1>
+                <p>React, React Router adn Flux for ultra responsive web apps.</p>
+            </div>
+        );
+    }
+});
+
+module.exports = Home;
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * Created by miguel on 14/04/17.
  */
 $ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
 
-var App = console.log('Hello world from Browserify');
-
-module.exports = App;
+React.render(<Home />, document.getElementById('app'));
